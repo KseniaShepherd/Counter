29dfb31c65c4a459286adbf8000a0a2095e718d9
Ксения Пастухова <ksenialukina@MacBook-Pro-Ksenia.local>
2023-08-25 16:05:44 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.scss";
 
 function App() {
+  const [state, setState] = useState(0);
+  const onClickPlus = () => {
+    setState(state + 1);
+    console.log(state)
+  };
+  const onClickMinus = () => {
+    setState(state - 1);
+    console.log(state)
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>Счетчик:</h2>
+        <h1>{state}</h1>
+        <button onClick={() => onClickMinus()} className="minus">
+          - Минус
+        </button>
+        <button onClick={() => onClickPlus()} className="plus">
+          Плюс +
+        </button>
+      </div>
     </div>
   );
 }
